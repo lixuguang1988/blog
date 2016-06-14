8fd140c3e1583fa44880983b1b0f82ffa717186e
lixuguang88 <lixuguang_1988@163.com>
2016-06-14 21:32:06 +0800

    Update jstree.js

--- jstree.js
+++ jstree.js
@@ -1,36 +1,52 @@
 var data = [
   {
     id : 1,
-    name : "菜单1"，
-    level :　１，
+    name : "菜单1",
+    level :　1,
     parentid : 0,
     url : "/menu1/"
   },
   {
     id : 2,
-    name : "菜单2"，
-    level :　１，
+    name : "菜单2",
+    level :　1,
     parentid : 0,
     url : "/menu2/"
   },
   {
     id : 21,
-    name : "菜单21"，
-    level :　2，
+    name : "菜单21",
+    level :　2,
     parentid : 2,
     url : "/menu1/21/"
   },
   {
     id : 22,
-    name : "菜单22"，
-    level :　2，
+    name : "菜单22",
+    level :　2,
     parentid : 0,
     url : "/menu1/22/"
   },  
   {
     id : 23,
-    name : "菜单23"，
-    level :　2，
+    name : "菜单23",
+    level :　2,
+    parentid : 0,
+    url : "/menu1/23/"
+  },
+  {
+    id : 3,
+    name : "菜单3",
+    level :　1,
+    parentid : 0,
+    url : "/menu3/"
+  }
+];
+
+var recordList = [] /*记录遍历的树*/,
+    str = '<ul class="js-tree">',
+    i = 0,
+len = data.length ;
     parentid : 0,
     url : "/menu1/23/"
   },
