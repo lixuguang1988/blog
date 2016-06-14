5498d6d6e4e46d4785376426e317cbe07363c23a
lixuguang88 <lixuguang_1988@163.com>
2016-06-14 21:22:28 +0800

    Update jstree.js

--- jstree.js
+++ jstree.js
@@ -26,8 +26,15 @@ var data = [
     level :　2，
     parentid : 0,
     url : "/menu1/22/"
+  },  
+  {
+    id : 23,
+    name : "菜单23"，
+    level :　2，
+    parentid : 0,
+    url : "/menu1/23/"
   },
-    {
+  {
     id : 3,
     name : "菜单3"，
     level :　１，
@@ -35,6 +42,53 @@ var data = [
     url : "/menu3/"
   }
 ];
+
+var recordList = [] /*记录遍历的树*/,
+    str = '<ul class="js-tree">',
+    i = 0,
+    len = data.length ;
+    
+    
+for( ; i < len; i++){
+  if(recordList.length === 0){
+    str += '<li><a href="' + data[i].url + '">' + data[i].name + '</a>';
+    recordList.push(data[i]);
+    continue;
+  }
+  
+  //子级菜单
+  if(recordList[recordList.length - 1]['level'] < data[i]['level']){
+    str += '<ul><li><a href="' + data[i].url + '">' + data[i].name + '</a>';
+    recordList.push(data[i]);
+    continue;
+  }  
+  
+  //同级菜单
+  if(recordList[recordList.length - 1]['level'] === data[i]['level']){
+    str += '</li><li><a href="' + data[i].url + '">' + data[i].name + '</a></li>';
+    continue;
+  }
+  
+  //回上一级菜单
+  if(recordList[recordList.length - 1]['level'] > data[i]['level']){
+    str += '</li><li><a href="' + data[i].url + '">' + data[i].name + '</a>';
+    recordList.shift();
+    continue;
+  }  
+}    
+    
+    str +='</ul> '
+    
+    console.log(str)
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
