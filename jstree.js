f406a96f574b9133de7cb5ec52380b8d6f1e29b8
lixuguang88 <lixuguang_1988@163.com>
2016-06-14 20:58:06 +0800

    Create jstree.js

---
+++ jstree.js
@@ -0,0 +1,41 @@
+var data = [
+  {
+    id : 1,
+    name : "菜单1"，
+    level :　１，
+    parentid : 0,
+    url : "/menu1/"
+  },
+  {
+    id : 2,
+    name : "菜单2"，
+    level :　１，
+    parentid : 0,
+    url : "/menu2/"
+  },
+  {
+    id : 21,
+    name : "菜单21"，
+    level :　2，
+    parentid : 2,
+    url : "/menu1/21/"
+  },
+  {
+    id : 22,
+    name : "菜单22"，
+    level :　2，
+    parentid : 0,
+    url : "/menu1/22/"
+  },
+    {
+    id : 3,
+    name : "菜单3"，
+    level :　１，
+    parentid : 0,
+    url : "/menu3/"
+  }
+];
+
+
+
+
