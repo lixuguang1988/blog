ff05661795fd88781d4158f5f70ead52341362f3
lixuguang88 <lixuguang_1988@163.com>
2016-08-14 15:55:16 +0800

    Create log.js

---
+++ ninja/02/log.js
@@ -0,0 +1,11 @@
+function log(){
+    try{
+        console.log.apply(console, arguments);
+    }catch(e){
+        try{
+            opera.postError.apply(opera, arguments); // 尝试使用opera的postError方法
+        }catch(e){
+            alert(Array.prototype.join.call(arguments, " ");
+        }
+    }
+}
