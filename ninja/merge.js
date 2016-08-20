d21f99913c7f1c10007ada7f4940a158f87304c3
lixuguang88 <lixuguang_1988@163.com>
2016-08-20 15:57:06 +0800

    Create merge.js

---
+++ ninja/merge.js
@@ -0,0 +1,8 @@
+function merge(root){
+    for(var i = 1; i < arguments.length; i++){
+        for(var prop in arguments[i]){
+            root[prop] = arguments[i][prop];
+        }
+    }
+    return root;
+}
