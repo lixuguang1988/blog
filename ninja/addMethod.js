b20a29840d6e67bc65df8f4c1599eb9f5eaa0195
lixuguang88 <lixuguang_1988@163.com>
2016-08-20 16:08:32 +0800

    Update addMethod.js

--- ninja/addMethod.js
+++ ninja/addMethod.js
@@ -5,12 +5,16 @@
  * fn     Function 函数
 **/
 functon addMehtod(object, name, fn){
+    //保存原来的函数
     var old = object[name];
     object[name] = function(){
+        //函数形参的数量等于实参的数量，就调用该函数
         if(fn.length == arguments.length){
-            return fn.apply(this, arguments)
+            return fn.apply(this, arguments);
+            //如果传入参数不匹配调用原来的方法
         }else if(typeof old === "function"){
             return old.apply(this, arguments);
         }
     }
 }
+
