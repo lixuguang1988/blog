eee074de3b8375da3b91ac13ad460580b58e730b
lixuguang88 <lixuguang_1988@163.com>
2016-08-20 16:19:33 +0800

    Create isPalindrome.js

---
+++ ninja/04/isPalindrome.js
@@ -0,0 +1,6 @@
+//判断给定的字符串是否是回文
+function isPalindrome(text){
+    if(text.length <= 1){ return true;}
+    if(text.charAt(0) != text.charAt(text.length-1){return false;}
+    return isPalindrome(text.substring(1, text.length - 1));
+}   
