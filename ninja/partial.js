774e9a5be61e3ca60bf5213b2b1fcdae310a8604
lixuguang88 <lixuguang_1988@163.com>
2016-08-25 16:13:28 +0800

    Update partial.js

--- ninja/partial.js
+++ ninja/partial.js
@@ -2,15 +2,17 @@
  * Created by lixuguang on 2016/8/25.
  */
 
-Function.prototype.partial = function(){
-    var fn = this, args = Array.prototype.slice.call(arguments);
-    return function(){
-        var arg = 0;
-        for(var i = 0; i < args.length && i < arguments.length; i++){
-            if(args[i] === undefined){
-                args[i] = arguments[arg++];
+if(!Function.prototype.partial) {
+    Function.prototype.partial = function () {
+        var fn = this, args = Array.prototype.slice.call(arguments);
+        return function () {
+            var arg = 0;
+            for (var i = 0; i < args.length && i < arguments.length; i++) {
+                if (args[i] === undefined) {
+                    args[i] = arguments[arg++];
+                }
             }
+            return fn.apply(this, args);
         }
-        return fn.apply(this, args);
-    }
-};
+    };
+}
