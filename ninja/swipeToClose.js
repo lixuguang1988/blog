42961f152964fd2991a5e353c4336e2fed219c1a
lixuguang88 <lixuguang_1988@163.com>
2017-12-20 12:44:04 +0800

    Update swipeToClose.js

--- ninja/swipeToClose.js
+++ ninja/swipeToClose.js
@@ -1,62 +1,41 @@
 var swipeToClose = {
   isPaneX : false,
-  touchStartX: 0, 
-  touchStartY: 0, 
+  startX: 0, 
+  startY: 0, 
+  threshold: 80, //滑动距离的阈值
+  thresholdFromX: 50, //从左侧开始滑动的阈值
   trackElem: [document.querySelector('#wrapper'), document.querySelector('#header'), document.querySelector('#footer')],
-  vendor: function(){
-    var vendors = ['t', 'webkitT', 'MozT', 'msT', 'OT'],
-      _elementStyle = document.createElement('div').style,
-      transform,
-      i = 0,
-      l = vendors.length;
-
-    for ( ; i < l; i++ ) {
-      transform = vendors[i] + 'ransform';
-      if ( transform in _elementStyle ) return vendors[i].substr(0, vendors[i].length-1);
-    }
-
-    return false;
-  }(),
-  prefixStyle: function(style) {
-    if ( this.vendor === false ) return false;
-    if ( this.vendor === '' ) return style;
-    return this.vendor + style.charAt(0).toUpperCase() + style.substr(1);
-  },
   startTrack: function(e){
     var touch  = e.changedTouches[0];
-    //两根手指 || 不从左侧滑动
-    if(e.changedTouches.length > 1 || touch.pageX > 80){
+    //不从左侧滑动 两根手指
+    if(touch.pageX > this.thresholdFromX || e.changedTouches.length > 1 ){
       this.isPaneX = false;
       return false;
     }
 
     this.isPaneX = true;
-    this.touchStartX = touch.pageX;
-    this.touchStartY = touch.pageY;
+    this.startX = touch.pageX;
+    this.startY = touch.pageY;
   },
   moveTrack: function(e){
     var touch= e.changedTouches[0], 
         tempStartX = touch.pageX,
-        tempStartY = touch.pageY,
-        diff;
-    //两根手指 || 不是左滑 || (向上滑幅度过大 ||  Math.abs(tempStartY - this.touchStartY) > 20)
-    console.log(tempStartY - this.touchStartY, 'diff Y')
+        tempStartY = touch.pageY;
+    //不是左滑 || 两根手指 ||  不处理了 (向上滑幅度过大 ||  Math.abs(tempStartY - this.startY) > 20)
     if(!this.isPaneX || e.changedTouches.length > 1 ){
       this.isPaneX = false;
       this.updateElemPosition(0, 0);
       return false;
     }
     
-    diff = tempStartX - this.touchStartX;
-    this.updateElemPosition(diff, 0);
+    this.updateElemPosition(tempStartX - this.startX, 0);
   },
   endTrack: function(e){
-    console.log(e)
     var touch= e.changedTouches[0], 
         tempStartX = touch.pageX,
         tempStartY = touch.pageY;
-    //两根手指 || 不是左滑 || 向上滑幅度过大 (||  tempStartY - this.touchStartY > 100) || 向左滑动幅度过小
-    if(!this.isPaneX || e.changedTouches.length > 1  || Math.abs(tempStartX - this.touchStartX) < 20){
+    //不是左滑 || 两根手指 || 向左滑动幅度过小
+    if(!this.isPaneX || e.changedTouches.length > 1  || tempStartX - this.startX < this.threshold){
       this.updateElemPosition(0, 0);
       return false;
     }
@@ -76,5 +55,24 @@ var swipeToClose = {
   cancelTrack: function(e){
     this.updateElemPosition(0, 0);
   },
-  callBack: null
+  callBack: null,
+  vendor: function(){
+    var vendors = ['t', 'webkitT', 'MozT', 'msT', 'OT'],
+      _elementStyle = document.createElement('div').style,
+      transform,
+      i = 0,
+      l = vendors.length;
+
+    for ( ; i < l; i++ ) {
+      transform = vendors[i] + 'ransform';
+      if ( transform in _elementStyle ) return vendors[i].substr(0, vendors[i].length-1);
+    }
+
+    return false;
+  }(),
+  prefixStyle: function(style) {
+    if ( this.vendor === false ) return false;
+    if ( this.vendor === '' ) return style;
+    return this.vendor + style.charAt(0).toUpperCase() + style.substr(1);
+  }
 }
