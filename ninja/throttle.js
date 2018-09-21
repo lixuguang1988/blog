fc3039a7271f965506d1456e1ed760a73e4c2c54
lixuguang88 <lixuguang_1988@163.com>
2018-09-21 13:02:49 +0800

    Create throttle.js

---
+++ ninja/throttle.js
@@ -0,0 +1,12 @@
+  // limit the number of events per second
+  function throttle(callback, delay) {
+    var previousCall = new Date().getTime();
+    return function() {
+      var time = new Date().getTime();
+
+      if ((time - previousCall) >= delay) {
+        previousCall = time;
+        callback.apply(null, arguments);
+      }
+    };
+  }
