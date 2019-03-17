e0111e2695b4acfdfe92e1beb667321b5f9ef45a
lixuguang1988 <lixuguang_1988@163.com>
2019-03-17 13:40:51 +0800

    Create dgit查看当前分支所属.m

---
+++ git/dgit查看当前分支所属.m
@@ -0,0 +1,28 @@
+1. 创建并切换到该分支
+
+git checkout -b xxx
+
+ 
+
+2. 查看远程分支
+git branch -a
+
+带有“*”号的表示当前分支 
+
+
+3. 查看本地分支
+
+git branch
+
+ 
+
+4. 切换分支命令
+
+# 切换回master
+
+git checkout master
+
+# 切换换其它分支
+
+git checkout xxx
+
